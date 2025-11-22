58fcc0e26ccf85cc81f66b01727caa9967a573d0
Dhruv Kumar Singh <dkmain12@gmail.com>
2025-11-22 19:18:14 +0530

    Register UploadHistory model in admin panel
    Added UploadHistory model to Django admin panel.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UploadHistory
 
-# Register your models here.
+# This tells the Admin panel: "Please show the UploadHistory table!"
+@admin.register(UploadHistory)
+class UploadHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'upload_time', 'total_count', 'avg_pressure') # Columns to show in the list
+    list_filter = ('upload_time',) # Add a filter sidebar
+    ordering = ('-upload_time',) # Newest first
